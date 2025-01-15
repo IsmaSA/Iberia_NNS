70f0788380fc647b390d7f35f08ea83a63f09811
Ismael Soto <isma-sa@hotmail.com>
2025-01-15 08:22:39 +0100

    Update Code_clean.r

--- Code_clean.r
+++ Code_clean.r
@@ -1,9 +1,10 @@
 
-# Code to use the clean database (LINUX) 
+### Code to use the clean database (LINUX) 
 
-## Ismael Soto  - University of South Bohemia in Ceske Budejovice (USB)
+# Ismael Soto --- University of South Bohemia in Ceske Budejovice (USB)
 Sys.time()
-ls()
+
+
 pacman::p_load(sf,dplyr,tidyr,xlsx,writexl,readxl,sp, ggplot2,terra,raster, rnaturalearth,rnaturalearthdata, readr,rgbif)
 
 ### Create the basic folders 
@@ -23,9 +24,19 @@ df <- readxl::read_xlsx(path = './Database/Iberia.xlsx')
 head(df)
 df = df %>% filter(Location %in% c('Spain', 'Portugal', 'Andorra', 'Gibraltar'))
 names(df)
-df1= df[!duplicated(df[,c('Location','GBIF_key')]), ]
-country = unique(df$Location)
+df= df[!duplicated(df[,c('Location','GBIF_key')]), ]
+df= df[df$GBIF_key != '0', ]
+df= df[!df$New_names %in% c('Ommatotriton sp.','Acridotheres sp.'), ]
 
-cat('Total species:', length(unique(df1$New_names)) )
+# Check for missing information
+nas <- sapply(df[, c("Family", "Class", "Phylum",'Group','Pathway_refined','Native_range')], function(column) {
+  sum(is.na(column))
+})
+nas
+
+
+cat('Total species:', length(unique(df1$New_names)) ) # 2,639 sp
+
+df %>% group_by(Location) %>% summarise(Species = n_distinct(New_names))
 
 
